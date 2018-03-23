a99c41a97cfed877afe8a12e4a4f9206011a6e18
Dennis Todoric <dennistodoric@Dennis-MacBook.local>
2018-03-23 14:41:40 +0100

    init

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "htwg.se.SlayerTheSpy"
+
+version := "0.1"
+
+scalaVersion := "2.12.5"
---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,9 @@
+package main.scala
+
+object HelloWorld {
+
+  def main(args: Array[String]): Unit = {
+    println("Bitte")
+  }
+
+}
